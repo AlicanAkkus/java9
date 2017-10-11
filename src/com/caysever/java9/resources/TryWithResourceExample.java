811ef5598b0124f8182cc6336669ce44791f56fe
Alican Akkus <alican.akkus@iyzico.com>
2017-10-11 14:12:46 +0300

    refactor resources example

--- src/com/caysever/java9/resources/TryWithResourceExample.java
+++ src/com/caysever/java9/resources/TryWithResourceExample.java
@@ -1,6 +1,7 @@
 package com.caysever.java9.resources;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 
 public class TryWithResourceExample {
@@ -8,7 +9,7 @@ public class TryWithResourceExample {
 
     public static void main(String[] args) throws Exception {
 
-        FileReader in = new FileReader("/Users/alican.akkus/workspace/iyzico-workspace/java9-sample/src/com/caysever/java9/resources/java9-feature");
+        FileReader in = new FileReader(new File("src/com/caysever/java9/resources", "java9-feature"));
         BufferedReader reader = new BufferedReader(in);
         try (reader; in) {
             reader.lines().forEach(System.err::println);
